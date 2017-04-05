8294fd662958ff9f74bb70709fad9d47ae46dc19
xu <541280996@qq.com>
2017-04-05 13:44:31 +0800

    test

---
+++ wejoy/resources/views/admin/index.blade.php
@@ -0,0 +1,11 @@
+@extends('/admin/layouts/app')
+@section('title','page Title')
+@section('top')
+    @include('/admin/public/top')
+@endsection
+@section('sidebar')
+    @include('/admin/public/sidebar')
+@endsection
+@section('content')
+    @include('/admin/public/content')
+@endsection
